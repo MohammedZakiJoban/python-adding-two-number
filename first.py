eea64706a3f384082b96a0f0f91496fcbf5e5302
Mohammed Joban <mohamedjoban@gmail.com>
2019-04-24 16:52:34 +0300

    first commit

---
+++ first.py
@@ -0,0 +1,20 @@
+def main():
+
+	
+	print("please enter a number") 
+	a  = int(input()) 
+	print("Please enter another number")
+	b = int(input())
+
+	
+	print("the total is"+ str(cal(a,b)))
+
+def cal(a,b):
+	x = a + b
+	return x
+
+
+main()
+
+
+
